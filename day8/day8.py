35f7aa278e6494e82449da56b01c67893f564cac
Matt Girard <mgirard@umassd.edu>
2019-08-12 14:06:14 -0400

    Adding complete solution

--- day8/day8.py
+++ day8/day8.py
@@ -1,21 +1,22 @@
 
-def solve():
-    input = [0,1,0,2,1,0,1,3,2,1,2,1]
-    height_difference_last = []
-    height_difference_max = []
+def solve(input):
     max_height = 0
-    total_rainwater = 0
+    rain_water_tracker = []
     for i in range(len(input)):
-        total_rainwater += max(0,max_height-input[i])
-        max_height = max(max_height,input[i])
-        if i == 0:
-            height_difference_last.append(0)
-            height_difference_max.append(0)
+        if input[i] < max_height:
+            new_max = max(input[i:])
+            if new_max >= max_height:
+                rain_water_tracker.append(max_height - input[i])
+            else:
+                rain_water_tracker.append(new_max - input[i])
         else:
-            height_difference_last.append(input[i] - input[i-1])
-            height_difference_max.append(input[i] - max_height)
-
-    print(total_rainwater)
+            max_height = max(max_height, input[i])
+            rain_water_tracker.append(0)
+    return rain_water_tracker
 
 if __name__ == "__main__":
-    solve()
+    input = [0, 3, 1, 2, 0, 0, 3, 1, 4, 1, 3, 2, 1, 0, 1, 0]
+    rain_water_tracker = solve(input)
+    print("Rain Water Results")
+    print(rain_water_tracker)
+    print("Total Units of Rain Collected: ", sum(rain_water_tracker))
